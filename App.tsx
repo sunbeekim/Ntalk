072d65c8019a530d938cf09aee27db121802a0f3
sunbeekim <62640672+sunbeekim@users.noreply.github.com>
2020-10-27 23:01:28 +0900

    first upload!

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+
+const CatApp = () => {
+  return (
+    <View>
+      <Image
+        source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
+        style={{width: 200, height: 200}}
+      />
+      <Text>Hello, I am your cat!</Text>
+    </View>
+  );
+}
+
+export default CatApp;
+export default CatApp;
